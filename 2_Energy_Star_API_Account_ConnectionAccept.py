45b46d281658e7449be903f114eb584ed243b68b
BenchmarkDC <32399536+BenchmarkDC@users.noreply.github.com>
2020-07-21 10:01:10 -0400

    Add files via upload

--- 2_Energy_Star_API_Account_ConnectionAccept.py
+++ 2_Energy_Star_API_Account_ConnectionAccept.py
@@ -3,7 +3,16 @@ from EnergyStarAPI import EnergyStarClient
 from pyo365 import MSGraphProtocol, Connection, Account, Message
 from validate_email import validate_email
 from datetime import timedelta, datetime
-import array, xlrd, csv, os, time, logging, timeit
+from ast import literal_eval
+import numpy as np
+import pandas as pd
+from email.mime.text import MIMEText
+from O365 import MSGraphProtocol, Connection, Account
+from O365.message import  Message
+import array, xlrd, csv, os, time, logging, timeit, sys, json, requests
+
+
+
 
 
  
@@ -25,91 +34,54 @@ output_csvfile = "CSV output/contact info_" + tstamp +".csv"
 filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'connectAccept.log')
 logging.basicConfig(filename=filename, level=logging.DEBUG, filemode='w')
 
-WebServices_ReferenceDoc = xlrd.open_workbook('WebServices_Reference_Document.XLSX')
-customFieldSetUp = WebServices_ReferenceDoc.sheet_by_index(2)
-liveEnvironmentAccounts = WebServices_ReferenceDoc.sheet_by_index(3)
-emailsToSend = WebServices_ReferenceDoc.sheet_by_index(7)
-PMUserName = liveEnvironmentAccounts.cell_value(1,1)
-PMPassword = liveEnvironmentAccounts.cell_value(1,2)
-o365UserName = liveEnvironmentAccounts.cell_value(3,1)
-o365Password = liveEnvironmentAccounts.cell_value(3,2)
-numberofRows = customFieldSetUp.nrows
+liveEnvironmentAccounts = pd.read_excel(open('WebServices_Reference_Document.XLSX', 'rb'), sheet_name='liveEnvironmentAccounts', dtype = str)  
+customFieldSetUp = pd.read_excel(open('WebServices_Reference_Document.XLSX', 'rb'), sheet_name='customFieldSetUp', dtype = str)  
+emailBodys = pd.read_excel(open('WebServices_Reference_Document.XLSX', 'rb'), sheet_name='emailBodys', dtype = str) 
+o365UserName = str(liveEnvironmentAccounts.loc[liveEnvironmentAccounts['Account Name']=='Office365','Account UserName'].values[0])
+o365Password = str(liveEnvironmentAccounts.loc[liveEnvironmentAccounts['Account Name']=='Office365','Account Password'].values[0])
+PMUserName = str(liveEnvironmentAccounts.loc[liveEnvironmentAccounts['Account Name']=='ENERGY STAR Account','Account UserName'].values[0])
+PMPassword = str(liveEnvironmentAccounts.loc[liveEnvironmentAccounts['Account Name']=='ENERGY STAR Account','Account Password'].values[0])
 
 ### Account Login
 ES_Client = EnergyStarClient(PMUserName,PMPassword)
 credentials = (o365UserName, o365Password)
 scopes = ["https://graph.microsoft.com/Mail.ReadWrite", "https://graph.microsoft.com/Mail.Send"]
 account = Account(credentials = credentials, scopes = scopes)
+account.connection.refresh_token
 
 print ("----Gets Pending List IDs/Accepts invitation/Gets Custom Field Information")
 
 accountIDs = ES_Client.get_pending_connection_list_multipage()
+Data_View = pd.DataFrame(list(set(accountIDs)), columns =  ['Account IDs'])
 
-accountIDs = list(set(accountIDs))
-
-print (accountIDs)
-
-i = 0
-while i < len(accountIDs):
+for index, item in Data_View.iterrows():
 	with open('xml-templates/connection.xml') as template_file:
-		ES_Client.post_accept_invite(template_file, accountIDs[i])
-	i += 1
-
-
-
-customFieldValues = []
-headers = []
-i = 0
-while i < len(accountIDs):
-	headers = []
-	headers.append('Account IDs')
-	entrylist = []
-	entrylist.append(accountIDs[i])
-	customdata = ES_Client.get_custom_field_data(accountIDs[i]).text
+		ES_Client.post_accept_invite(template_file, item['Account IDs'])
+	
+	customdata = ES_Client.get_custom_field_data(item['Account IDs']).text
 	root = Et.fromstring(customdata)
-	b = 1
-	while b < numberofRows:
-		for Child in root.findall(".//*[@name='"+customFieldSetUp.cell_value(b,8)+"']"):
-			entrylist.append(Child.text)
-			headers.append(customFieldSetUp.cell_value(b,8))
-		b += 1
-	customFieldValues.append(entrylist)
+	for item2 in customFieldSetUp['textAttribute0.3']:
+		for Child in root.findall(".//*[@name='"+item2+"']"):
+			Data_View.loc[index,item2] = Child.text
+			print (Child.text)
+			print (item2)
 
-	i += 1
+				
+print (Data_View)
+
+for index, item in Data_View.iterrows():
+	is_valid = validate_email(item['Email Address'], verify = True)
+	print (item['Email Address'])
+	if is_valid != False:
+		messageSubject = emailBodys.loc[emailBodys['Email Type'] == 'Account Share Accepted','Subject'].values[0]
+		messageBody = emailBodys.loc[emailBodys['Email Type'] == 'Account Share Accepted','Body'].values[0]
+		messageBody = messageBody.replace('[NAME]', item['Contact Name'])
+		m = account.new_message()
+		m.sender.address = 'info.benchmark@DC.gov'
+		m.to.add(item['Email Address'])
+		m.subject = messageSubject
+		m.body = messageBody
+		m.send()		
 
 
-with open(output_csvfile,'w') as output:
-	writer = csv.writer(output, lineterminator='\n')
-	writer.writerow(headers)
-	writer.writerows(customFieldValues)
 
-
-
-contactName = []
-contactEmail = []
-account.connection.refresh_token
-
-i = 0
-with open(output_csvfile,newline='') as CL:
-	reader = csv.DictReader(CL)
-	for row in reader:
-		contactName.append(row['Contact Name'])
-		contactEmail.append(row['Email Address'])
-		print (contactName, contactEmail)
-	while i < len(contactEmail):
-		messageSubject = emailsToSend.cell_value(1,1)
-		messageBody = emailsToSend.cell_value(1,2)
-		is_valid = validate_email(contactEmail[i], verify = True)
-		print (is_valid)
-		if is_valid != False:
-			logging.warning('message will be sent')
-			m = account.new_message()
-			messageBody = messageBody.replace('[NAME]', contactName[i])
-			m.sender.address = 'info.benchmark@DC.gov'
-			m.to.add(contactEmail[i])
-			m.subject = messageSubject
-			m.body = messageBody
-			m.send()
-			i += 1
-		else:
-			i += 1
--- 3_Energy_Star_API_Property_ConnectionAcceptReject.py
+++ 3_Energy_Star_API_Property_ConnectionAcceptReject.py
@@ -1,10 +1,16 @@
 import xml.etree.ElementTree as Et
 from EnergyStarAPI import EnergyStarClient
-from ast import literal_eval
 from pyo365 import MSGraphProtocol, Connection, Account, Message
 from validate_email import validate_email
 from datetime import timedelta, datetime
-import array, xlrd, csv, os, smtplib, time, logging
+from ast import literal_eval
+import numpy as np
+import pandas as pd
+from fuzzywuzzy import fuzz, process
+from email.mime.text import MIMEText
+from O365 import MSGraphProtocol, Connection, Account
+from O365.message import  Message
+import array, xlrd, csv, os, time, logging, timeit, sys, json, requests
 
 filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'propertyConnectAccept.log')
 logging.basicConfig(filename=filename, level=logging.DEBUG, filemode='w')
@@ -21,112 +27,79 @@ temp = "CSV output/temp.csv"
 date_format = "%Y%m%d_%H%M%S"
 date_End_temp = datetime.now()
 tstamp = date_End_temp.strftime(date_format)
-accepted_csvfile = "CSV output/properties accepted_" + tstamp +".csv"
-rejected_csvfile = "CSV output/properties rejected_" + tstamp +".csv"
 
-WebServices_ReferenceDoc = xlrd.open_workbook('WebServices_Reference_Document.XLSX')
-customFieldSetUp = WebServices_ReferenceDoc.sheet_by_index(2)
-liveEnvironmentAccounts = WebServices_ReferenceDoc.sheet_by_index(3)
-DCBuildingList = WebServices_ReferenceDoc.sheet_by_index(4)
-emailsToSend = WebServices_ReferenceDoc.sheet_by_index(7)
-PMUserName = liveEnvironmentAccounts.cell_value(1,1)
-PMPassword = liveEnvironmentAccounts.cell_value(1,2)
-o365UserName = liveEnvironmentAccounts.cell_value(3,1)
-o365Password = liveEnvironmentAccounts.cell_value(3,2)
-numberofRows = customFieldSetUp.nrows
-numberofCoveredBuildings = DCBuildingList.nrows
+
+liveEnvironmentAccounts = pd.read_excel(open('WebServices_Reference_Document.XLSX', 'rb'), sheet_name='liveEnvironmentAccounts', dtype = str)  
+customFieldSetUp = pd.read_excel(open('WebServices_Reference_Document.XLSX', 'rb'), sheet_name='customFieldSetUp', dtype = str)  
+emailBodys = pd.read_excel(open('WebServices_Reference_Document.XLSX', 'rb'), sheet_name='emailBodys', dtype = str) 
+DCBuildingList = pd.read_excel(open('WebServices_Reference_Document.XLSX', 'rb'), sheet_name='buildingListCurrent', dtype = str) 
+o365UserName = str(liveEnvironmentAccounts.loc[liveEnvironmentAccounts['Account Name']=='Office365','Account UserName'].values[0])
+o365Password = str(liveEnvironmentAccounts.loc[liveEnvironmentAccounts['Account Name']=='Office365','Account Password'].values[0])
+PMUserName = str(liveEnvironmentAccounts.loc[liveEnvironmentAccounts['Account Name']=='ENERGY STAR Account','Account UserName'].values[0])
+PMPassword = str(liveEnvironmentAccounts.loc[liveEnvironmentAccounts['Account Name']=='ENERGY STAR Account','Account Password'].values[0])
 
 
 
 ### Web Services Login
 ES_Client = EnergyStarClient(PMUserName,PMPassword)
 credentials = (o365UserName, o365Password)
-account = Account(credentials = credentials)
+scopes = ["https://graph.microsoft.com/Mail.ReadWrite", "https://graph.microsoft.com/Mail.Send"]
+account = Account(credentials = credentials, scopes = scopes)
 account.connection.refresh_token
 
-DCRealPropertyIDs =[]
-
-i = 1
-while i < numberofCoveredBuildings:
-	value1 = DCBuildingList.cell_value(i,0)
-	DCRealPropertyIDs.append(value1)
-	i += 1
-
-propertyIDstoAccept = ES_Client.get_pending_propertyconnection_list_multipage_accept(DCRealPropertyIDs)
-propertyIDstoReject = ES_Client.get_pending_propertyconnection_list_multipage_reject(DCRealPropertyIDs)
-
-propertyIDstoAccept = list(set(propertyIDstoAccept))
-propertyIDstoReject = list(set(propertyIDstoReject))
+pendingPropertyShares = ES_Client.get_pending_propertyconnection_list_multipage()
 
 
-with open(accepted_csvfile,'w') as output:
-	writer = csv.writer(output, lineterminator='\n')
-	writer.writerow(['Accounts Accepted'])
-	writer.writerows(propertyIDstoAccept)
-
-with open(rejected_csvfile,'w') as output:
-	writer = csv.writer(output, lineterminator='\n')
-	writer.writerow(['Accounts Rejected'])
-	writer.writerows(propertyIDstoReject)
+for index, item in pendingPropertyShares.iterrows():
+	for ID in DCBuildingList['DC Real Property  Unique ID']:
+		ratio = process.extractOne(str(item['ReportedDCID']), ID)
+		if item['PropertyID'] == ID or ratio[1] >= 85:
+			item['acccept/reject'] = 'accept'
+			with open('xml-templates/property_accept_connection.xml') as template_file:
+				ES_Client.post_property_response(template_file, item['PropertyID'])
+			propertyContactName = ''.join(ES_Client.get_Property_ContactName(item['PropertyID']))
+			propertyContactEmail = ''.join(ES_Client.get_Property_ContactEmail(item['PropertyID']))
+			propertyDCRealID = ''.join(ES_Client.get_DCRealID(item['PropertyID']))
+			propertyName = ''.join(ES_Client.get_property_name(item['PropertyID']))
+			messageSubject = emailBodys.loc[emailBodys['Email Type'] == 'Property Share Accepted','Subject'].values[0]
+			messageBody = emailBodys.loc[emailBodys['Email Type'] == 'Property Share Accepted','Body'].values[0]
+			is_valid = validate_email(propertyContactEmail, verify = True)
+			if is_valid != False:
+				messageBody = messageBody.replace('[NAME]', propertyContactName)
+				messageBody = messageBody.replace('[PROPERTY NAME]', propertyName)
+				messageBody = messageBody.replace('[UBI]', propertyDCRealID)
+				m = account.new_message()
+				m.sender.address = 'info.benchmark@DC.gov'
+				m.to.add(propertyContactEmail)
+				m.subject = messageSubject
+				m.body = messageBody
+				m.send()
+	else:
+		item['acccept/reject'] = 'reject'
+		with open('xml-templates/property_reject_connection.xml') as template_file:
+			ES_Client.post_property_response(template_file, item['PropertyID'])
+		propertyContactName = ''.join(ES_Client.get_Property_ContactName(item['PropertyID']))
+		propertyContactEmail = ''.join(ES_Client.get_Property_ContactEmail(item['PropertyID']))
+		propertyDCRealID = ''.join(ES_Client.get_DCRealID(item['PropertyID']))
+		propertyName = ''.join(ES_Client.get_property_name(item['PropertyID']))
+		messageSubject = emailBodys.loc[emailBodys['Email Type'] == 'Property Share Rejected','Subject'].values[0]
+		messageBody = emailBodys.loc[emailBodys['Email Type'] == 'Property Share Rejected','Body'].values[0]
+		is_valid = validate_email(propertyContactEmail, verify = True)
+		if is_valid != False:
+			messageBody = messageBody.replace('[NAME]', propertyContactName)
+			messageBody = messageBody.replace('[PROPERTY NAME]', propertyName)
+			messageBody = messageBody.replace('[UBI]', propertyDCRealID)
+			m = account.new_message()
+			m.sender.address = 'info.benchmark@DC.gov'
+			m.to.add(propertyContactEmail)
+			m.bcc.add('JoAnna.Saunders@dc.gov')
+			m.subject = messageSubject
+			m.body = messageBody
+			m.send()
 
 
 print ("----Accepts/Rejects invitation")
 
-i = 0
-while i < len(propertyIDstoAccept):
-	with open('xml-templates/property_accept_connection.xml') as template_file:
-			ES_Client.post_property_response(template_file, propertyIDstoAccept[i])
-	propertyContactName = ''.join(ES_Client.get_Property_ContactName(propertyIDstoAccept[i]))
-	propertyContactEmail = ''.join(ES_Client.get_Property_ContactEmail(propertyIDstoAccept[i]))
-	propertyDCRealID = ''.join(ES_Client.get_DCRealID(propertyIDstoAccept[i]))
-	propertyName = ''.join(ES_Client.get_property_name(propertyIDstoAccept[i]))
-	acceptMessageSubject = emailsToSend.cell_value(2,1)
-	is_valid = validate_email(propertyContactEmail, verify = True)
-	if is_valid != False:
-		acceptMessageBody = emailsToSend.cell_value(2,2)
-		acceptMessageBody = acceptMessageBody.replace('[NAME]', propertyContactName)
-		acceptMessageBody = acceptMessageBody.replace('[PROPERTY NAME]', propertyName)
-		acceptMessageBody = acceptMessageBody.replace('[UBI]', propertyDCRealID)
-		m = account.new_message()
-		m.sender.address = 'info.benchmark@DC.gov'
-		m.to.add(propertyContactEmail)
-		m.subject = acceptMessageSubject
-		m.body = acceptMessageBody
-		m.send()
-		i += 1
-	else:
-		i += 1
-
-
-i = 0
-while i < len(propertyIDstoReject):
-	with open('xml-templates/property_reject_connection.xml') as template_file:
-		ES_Client.post_property_response(template_file, propertyIDstoReject[i])
-	propertyContactName = ''.join(ES_Client.get_Property_ContactName(propertyIDstoReject[i]))
-	propertyContactEmail = ''.join(ES_Client.get_Property_ContactEmail(propertyIDstoReject[i]))
-	propertyDCRealID = ''.join(ES_Client.get_DCRealID(propertyIDstoReject[i]))
-	propertyName = ''.join(ES_Client.get_property_name(propertyIDstoReject[i]))
-	is_valid = validate_email(propertyContactEmail, verify = True)
-	if is_valid != False:
-		rejectMessageSubject = emailsToSend.cell_value(3,1)
-		rejectMessageBody = emailsToSend.cell_value(3,2)
-		rejectMessageBody = rejectMessageBody.replace('[NAME]', propertyContactName)
-		rejectMessageBody = rejectMessageBody.replace('[PROPERTY NAME]', propertyName)
-		rejectMessageBody = rejectMessageBody.replace('[UBI]', propertyDCRealID)
-		m = account.new_message()
-		m.sender.address = 'info.benchmark@DC.gov'
-		m.to.add(propertyContactEmail)
-		m.subject = rejectMessageSubject
-		m.body = rejectMessageBody
-		m.send()
-		with open('xml-templates/property_disconnection.xml') as template_file:
-			ES_Client.post_property_disconnect(template_file, propertyIDstoReject[i])
-		i +=1
-	else:
-
-		with open('xml-templates/property_disconnection.xml') as template_file:
-			ES_Client.post_property_disconnect(template_file, propertyIDstoReject[i])
-		i +=1	
 
 
 ### Accepts all Meter Requests
@@ -137,8 +110,11 @@ accountIDs = ES_Client.get_customer_list()
 
 i = 0
 while i <len(accountIDs):
-	propertyIDs.extend(ES_Client.get_property_list(accountIDs[i]))
-	i += 1
+	try:
+		propertyIDs.extend(ES_Client.get_property_list(accountIDs[i]))
+		i += 1
+	except:
+		i += 1
 
 meterIDstoAccept = ES_Client.get_pending_meterconnection_list_multipage_accept(propertyIDs)
 
